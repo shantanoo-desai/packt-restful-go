f9e281b5deb57531456769c735b9cd9875e2d7ea
Shantanoo 'Shan' Desai <shantanoo.desai@gmail.com>
2023-11-21 15:52:38 +0100

    feat: add Echo based APIw
    Signed-off-by: Shantanoo 'Shan' Desai <shantanoo.desai@gmail.com>

---
+++ echo/main.go
@@ -0,0 +1,225 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/shantanoo-desai/packt-restful-go/cache"
+	"github.com/shantanoo-desai/packt-restful-go/user"
+	"gopkg.in/mgo.v2/bson"
+	"github.com/asdine/storm"
+)
+
+type response map[string]interface{}
+
+func serveCache(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if cache.Serve(c.Response(), c.Request()) {
+			return nil
+	}
+	return next(c)
+}
+}
+
+func cacheResponse(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+			c.Response().Writer = cache.NewWriter(c.Response().Writer, c.Request())
+			return next(c)
+	}
+}
+
+func usersOptions(c echo.Context) error {
+	methods := []string{ http.MethodGet, http.MethodPost, http.MethodHead, http.MethodOptions}
+	c.Response().Header().Set("Allow", strings.Join(methods, ","))
+	return c.NoContent(http.StatusOK)
+}
+
+func userOptions(c echo.Context) error {
+	methods := []string{ http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, http.MethodHead, http.MethodOptions}
+	c.Response().Header().Set("Allow", strings.Join(methods, ","))
+	return c.NoContent(http.StatusOK)
+}
+
+
+
+func usersGetAll(c echo.Context) error {
+	users, err := user.All()
+	if err != nil {
+		return echo.NewHTTPError( http.StatusInternalServerError)
+	}
+	if c.Request().Method == http.MethodHead {
+		return c.NoContent(http.StatusOK)
+
+	}
+	return c.JSON(http.StatusOK, response{"users": users})
+}
+
+func usersPostOne(c echo.Context) error {
+	u := new(user.User)
+
+	err := c.Bind(u)
+	if err != nil {
+		return echo.NewHTTPError( http.StatusBadRequest)
+	}
+	u.ID = bson.NewObjectId()
+	err = u.Save()
+	if err != nil {
+		if err == user.ErrRecordInvalid {
+			return echo.NewHTTPError( http.StatusBadRequest)
+		} else {
+			return echo.NewHTTPError( http.StatusInternalServerError)
+		}
+
+	}
+	cache.Drop("/users")
+	c.Response().Header().Set("Location", "/users/" + u.ID.Hex())
+	return c.NoContent(http.StatusCreated)
+}
+
+func usersGetOne(c echo.Context) error {
+	if cache.Serve(c.Response(), c.Request()) {
+		return nil
+	}
+	if !bson.IsObjectIdHex(c.Param("id")) {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+
+	id := bson.ObjectIdHex(c.Param("id"))
+
+	u, err := user.One(id)
+	if err != nil {
+		if err == storm.ErrNotFound {
+			return echo.NewHTTPError( http.StatusNotFound)
+		}
+		return echo.NewHTTPError( http.StatusInternalServerError)
+	}
+
+	if c.Request().Method == http.MethodHead {
+		return c.NoContent(http.StatusOK)
+
+	}
+	return c.JSON(http.StatusOK, response{"user": u})
+}
+
+func usersPutOne(c echo.Context) error {
+	u := new(user.User)
+
+	err := c.Bind(u)
+	if err != nil {
+		return echo.NewHTTPError( http.StatusBadRequest)
+	}
+	if !bson.IsObjectIdHex(c.Param("id")) {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+
+	id := bson.ObjectIdHex(c.Param("id"))
+
+	u.ID = id
+	err = u.Save()
+	if err != nil {
+		if err == user.ErrRecordInvalid {
+			return echo.NewHTTPError( http.StatusBadRequest)
+		} else {
+			return echo.NewHTTPError( http.StatusInternalServerError)
+		}
+
+	}
+
+	cache.Drop("/users")
+	return c.JSON(http.StatusOK, response{"user": u})
+}
+
+func usersPatchOne(c echo.Context) error {
+	if !bson.IsObjectIdHex(c.Param("id")) {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+
+	id := bson.ObjectIdHex(c.Param("id"))
+
+	u, err := user.One(id)
+	if err != nil {
+		if err == user.ErrRecordInvalid {
+			return echo.NewHTTPError( http.StatusNotFound)
+		}
+		return echo.NewHTTPError( http.StatusInternalServerError)
+	}
+	err = c.Bind(u)
+	if err != nil {
+		return echo.NewHTTPError( http.StatusBadRequest)
+	}
+	u.ID = id
+	err = u.Save()
+	if err != nil {
+		if err == user.ErrRecordInvalid {
+			return echo.NewHTTPError( http.StatusBadRequest)
+		} else {
+			return echo.NewHTTPError( http.StatusInternalServerError)
+		}
+
+	}
+
+	cache.Drop("/users")
+	return c.JSON( http.StatusOK, response{"user": u})
+}
+
+func usersDeleteOne(c echo.Context) error {
+	if !bson.IsObjectIdHex(c.Param("id")) {
+		return echo.NewHTTPError(http.StatusNotFound)
+	}
+
+	id := bson.ObjectIdHex(c.Param("id"))
+
+	err := user.Delete(id)
+	if err != nil {
+		if err == storm.ErrNotFound {
+			return echo.NewHTTPError( http.StatusNotFound)
+		}
+		return echo.NewHTTPError( http.StatusInternalServerError)
+	}
+	cache.Drop("/users")
+	cache.Drop(cache.MakeResource(c.Request()))
+	return c.NoContent(http.StatusOK)
+}
+
+
+
+func root(c echo.Context) error {
+	return c.String(http.StatusOK, "Running API v1")
+}
+
+func auth(username, password string, c echo.Context) (bool, error) {
+	if username == "joe" && password == "secret" {
+		return true, nil
+	}
+	return false, nil
+}
+
+func main() {
+	e := echo.New()
+	e.Pre(middleware.RemoveTrailingSlash())
+	e.Pre(middleware.Recover())
+	e.Pre(middleware.Secure())
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status} latency=${latency_human} \n",
+	}))
+	e.GET("/", root)
+
+	u := e.Group("/users")
+
+	u.OPTIONS("", usersOptions)
+	u.HEAD("", usersGetAll, serveCache)
+	u.GET("", usersGetAll, serveCache, cacheResponse)
+	u.POST("", usersPostOne, middleware.BasicAuth(auth))
+
+	uid := u.Group("/:id")
+	uid.OPTIONS("", userOptions)
+	uid.HEAD("", usersGetOne, serveCache)
+	uid.GET("", usersGetOne, serveCache, cacheResponse)
+	uid.PUT("", usersPutOne, middleware.BasicAuth(auth), cacheResponse)
+	uid.PATCH("", usersPatchOne, middleware.BasicAuth(auth), cacheResponse)
+	uid.DELETE("", usersDeleteOne, middleware.BasicAuth(auth))
+
+	e.Logger.Fatal(e.Start(":12345"))
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,18 @@ go 1.21.3
 
 require (
 	github.com/asdine/storm v2.1.2+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/echo/v4 v4.11.3 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.etcd.io/bbolt v1.3.8 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
